1893697c8ae9c73fe17c98120c2b8ec060634f44
Aisha Blfgeh <a.blfgeh1@ncl.ac.uk>
2016-04-19 18:34:22 +0100

    adding functions and comments

--- src/apc/core-lazyseq.clj
+++ src/apc/core-lazyseq.clj
@@ -16,8 +16,8 @@
                           :D :class3 :E :class4 :F :class5
                           :G :class6 :H :class7}))))
 
-
-(defontology apc
+;; 
+(defontology apc1
   :comment "This is the manual developed ontology")
 (defclass GroupName)
 (defclass CellName)
@@ -30,6 +30,12 @@
 (defoproperty hasLocation)
 (defoproperty fromGroup :domain GroupName)
 
+(defclass Autologous :super CellOrigin)
+(defclass Allogeneic :super CellOrigin :disjoint Autologous)
+(defpartition CellOrigin
+  [Allogeneic Autologous]
+  :super CellName)
+
 ;; save workbook in a variable and sheet1 
 (def  workbook (load-workbook "APC catalogue v5.xlsx"))
 (def sheet (select-sheet "Table 1" workbook))
@@ -39,30 +45,41 @@
 (def cell-try
   (->> (read-cell (select-cell "B3" (first(sheet-seq workbook)))
         ))) ; works fine
-
 (def sheet-test (cell-seq sheet)) ;save lazyseq of all cells in a sheet
 
-;; extract one row (groups in this case) but it saved into lazyseq of objects
-(def row-test (cell-seq (take 1 (drop 2 (row-seq sheet)))))
+;; this function extract row information into a lazy sequence of strings
+;; also removes spaces and first value of the row
+(defn row-info1
+  [sheet row]
+  (map clojure.string/trim
+       (remove nil?
+               (rest (doall (map read-cell
+                                 (cell-seq(take 1(drop row
+                                                       (row-seq sheet))))))))))
+;(row-info sheet 6)
 
-;; save groups' name as str in a lazyseq, need to remove spaces!
-(def groups-str
-  (map read-cell row-test))
-;; define groups str without extra free spaces in name and remove first 
-(def groups
-  (map clojure.string/trim (rest groups-str)))
+;; Those are the rows
+(def cell-nm (concat (row-info1 sheet 1) (row-info1 sheet 15))) ; +15
+(def groups (concat (row-info1 sheet 2) (row-info1 sheet 16)))  ; +16
+(def cl-ds (concat (row-info1 sheet 4) (row-info1 sheet 18)))   ; +18
+(def species (concat (row-info1 sheet 6) (row-info1 sheet 20))) ; +20
+(def cell-tp (concat (row-info1 sheet 7) (row-info1 sheet 21))) ; +21
+(def ant-ld (concat (row-info1 sheet 10) (row-info1 sheet 24))) ; +24
+(def cell-org (concat (row-info1 sheet 11) (row-info1 sheet 25))); +25
+(def st-mtr (concat (row-info1 sheet 12) (row-info1 sheet 26))) ; +26
+(def isol (concat (row-info1 sheet 13) (row-info1 sheet 27)))   ; +27
 
-;; extract row of cell names
-(def cell-row (cell-seq (take 1 (drop 1 (row-seq sheet)))))
+;; define individual of each string in the sequence
+(defn individuals [set-name cl-type]
+  (individual set-name
+              :type cl-type))
+;; test the function but got error:
+;; "Expect individual got lazyseq"
+;(def group-test (doall (map (individuals groups GroupName))))
 
-;; extract the value of cells names
-(def cellsName-str
-  (map read-cell cell-row))
-;; remove spaces and nils
-(def cellsName
-  (map clojure.string/trim (remove nil? cellsName-str)))
 
-;; this to create individuals of groups
+
+;;  to create individuals of groups
 (defn group [group-name]
   ;; group-name in B4
   (individual group-name
@@ -73,8 +90,8 @@
 (defn cell-name [name]
   (individual name
               :type CellName))
-(map cell-name cellsName)            ;test function
-(def cn (map cell-name cellsName)); save cell names in seq
+(map cell-name cell-nm)            ;test function
+(def cn (map cell-name cell-nm)); save cell names in seq
 
 ;; create cell line with all info
 (defn cell-line [cell-name group]
@@ -84,8 +101,21 @@
 
 ;; save all cell lines in one place
 (def lines
-  (map cell-line cn gr))
+  (doall
+   (map cell-line cn gr)))
 
-;(defindividual ATDC (lines 1)); lazyseq connot be cast to fn
+;; this suppose to define cell origins available in the sheet
+;; as individuals
+(def cell-origins
+  (individual
+    (doall
+     (map
+      #({"Autologous" Autologous
+         "Allogeneic" Allogeneic} %)
+      (cell-org))))) ;lazyseq cannot be cast to IFn
+
+(defindividual ATDC (first lines))
+
+(save-ontology "apc1.owl" :owl)
 
 (reasoner-factory :hermit)

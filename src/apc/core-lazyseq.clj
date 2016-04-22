80894c86c88d9391ae5f85b33c23868b2bd6d967
Aisha Blfgeh <a.blfgeh1@ncl.ac.uk>
2016-04-22 12:33:14 +0100

    add comments and descriptions

--- src/apc/core-lazyseq.clj
+++ src/apc/core-lazyseq.clj
@@ -3,18 +3,8 @@
         [tawny.pattern]
         [tawny.repl]
         [tawny.reasoner]
-        [dk.ative.docjure.spreadsheet]
-        [clojure.string :as str]))
-
-(defn -main
-  [& args]
-  ;;read table1 from excel sheet and save it in vector of maps
-  (def table1
-    (->> (load-workbook "APC catalogue v5.xlsx")
-         (select-sheet "Table 1")
-         (select-columns {:A :property :B :class1 :C :class2
-                          :D :class3 :E :class4 :F :class5
-                          :G :class6 :H :class7}))))
+        [dk.ative.docjure.spreadsheet])
+  (:require [clojure.string :as str]))
 
 ;; 
 (defontology apc1
@@ -36,62 +26,67 @@
   [Allogeneic Autologous]
   :super CellName)
 
-;; save workbook in a variable and sheet1 
-(def  workbook (load-workbook "APC catalogue v5.xlsx"))
+(refine Allogeneic :comment "Allogeneic is a word I don't know")
+
+;; save workbook in a variable and sheet1
+(def workbook (load-workbook "APC catalogue v5.xlsx"))
 (def sheet (select-sheet "Table 1" workbook))
 ;; define the first cell line column in one variable
 (def cell-line1 (select-columns {:B :line1} sheet))
-;; try to read one specific cell "B3" in this case
-(def cell-try
-  (->> (read-cell (select-cell "B3" (first(sheet-seq workbook)))
-        ))) ; works fine
+
 (def sheet-test (cell-seq sheet)) ;save lazyseq of all cells in a sheet
 
 ;; this function extract row information into a lazy sequence of strings
 ;; also removes spaces and first value of the row
-(defn row-info1
-  [sheet row]
-  (map clojure.string/trim
-       (remove nil?
-               (rest (doall (map read-cell
-                                 (cell-seq(take 1(drop row
-                                                       (row-seq sheet))))))))))
-;(row-info sheet 6)
 
-;; Those are the rows
-(def cell-nm (concat (row-info1 sheet 1) (row-info1 sheet 15))) ; +15
-(def groups (concat (row-info1 sheet 2) (row-info1 sheet 16)))  ; +16
-(def cl-ds (concat (row-info1 sheet 4) (row-info1 sheet 18)))   ; +18
-(def species (concat (row-info1 sheet 6) (row-info1 sheet 20))) ; +20
-(def cell-tp (concat (row-info1 sheet 7) (row-info1 sheet 21))) ; +21
-(def ant-ld (concat (row-info1 sheet 10) (row-info1 sheet 24))) ; +24
-(def cell-org (concat (row-info1 sheet 11) (row-info1 sheet 25))); +25
-(def st-mtr (concat (row-info1 sheet 12) (row-info1 sheet 26))) ; +26
-(def isol (concat (row-info1 sheet 13) (row-info1 sheet 27)))   ; +27
+;; row-to-blank
+
+(defn row-info
+  [s row]
+  (doall
+   (map clojure.string/trim
+        (remove nil?
+                (rest
+                 (map read-cell
+                      (cell-seq
+                       (take 1
+                             (drop row
+                                   (row-seq s))))))))))
+
+;; Those are the rows of individuals
+(def cell-nm
+  "The names of the cell lines."
+  (concat (row-info sheet 1) (row-info sheet 15))) ;
+
+(def groups (concat (row-info sheet 2) (row-info sheet 16)))
+(def cl-ds (concat (row-info sheet 4) (row-info sheet 18)))
+(def species (concat (row-info sheet 6) (row-info sheet 20))) ; partition
+(def cell-tp (concat (row-info sheet 7) (row-info sheet 21)))
+(def ant-ld (concat (row-info sheet 10) (row-info sheet 24)))
+(def cell-org (concat (row-info sheet 11) (row-info sheet 25))); partition
+(def st-mtr (concat (row-info sheet 12) (row-info sheet 26))) ; partition
+(def isol (concat (row-info sheet 13) (row-info sheet 27)))
+
+;;rows to be properties in the ontology
+(def loc (concat (row-info sheet 3) (row-info sheet 17)))
+(def status (concat (row-info sheet 5) (row-info sheet 19)))
+(def desc (concat (row-info sheet 8) (row-info sheet 22)))
+(def act (concat (row-info sheet 9) (row-info sheet 23)))
 
 ;; define individual of each string in the sequence
-(defn individuals [set-name cl-type]
-  (individual set-name
-              :type cl-type))
-;; test the function but got error:
-;; "Expect individual got lazyseq"
-;(def group-test (doall (map (individuals groups GroupName))))
+(defn individual-with-type
+  "Given something and something else return something.
 
-
-
+  I-NAME is the individual name.
+  I-TYPE is the type of the individual"
+  [i-name i-type]
+  (individual i-name :type i-type))
 ;;  to create individuals of groups
-(defn group [group-name]
-  ;; group-name in B4
-  (individual group-name
-              :type GroupName))
-(map group groups) ; test the function
-(def gr (map group groups)); save groups in seq
+(def group-test
+  (doall (map #(individual-with-type % GroupName) groups)))
 ;; this to create individuals of cell names
-(defn cell-name [name]
-  (individual name
-              :type CellName))
-(map cell-name cell-nm)            ;test function
-(def cn (map cell-name cell-nm)); save cell names in seq
+(def cell-name-test
+  (doall (map #(individual-with-type % CellName) cell-nm)))
 
 ;; create cell line with all info
 (defn cell-line [cell-name group]
@@ -112,10 +107,10 @@
      (map
       #({"Autologous" Autologous
          "Allogeneic" Allogeneic} %)
-      (cell-org))))) ;lazyseq cannot be cast to IFn
+      cell-org))))
 
 (defindividual ATDC (first lines))
 
 (save-ontology "apc1.owl" :owl)
-
+(save-ontology "apc1.omn" :omn)
 (reasoner-factory :hermit)
